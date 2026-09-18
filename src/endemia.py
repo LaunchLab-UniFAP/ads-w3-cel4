2b01accc5bb8ebb6550813a77337b63cb72bb2d3
Pedrohenrqs <henrique.o.4456@gmail.com>
2026-09-18 14:18:07 -0300

    feat: implementa calculo linear de focos

--- src/endemia.py
+++ src/endemia.py
@@ -1,17 +1,14 @@
 # LaunchLab UniFAP - Desenvolvimento ADS
 # Código de projeção de focos epidemiológicos
 
-def calcular_projecao_focos(focos_atuais, taxa_reproducao):
-    print("--- MONITORAMENTO DE ENDEMIAS UniFAP ---
-")
-    
-    # O conflito intencional:
-    # Aluno A implementará uma lógica baseada em crescimento linear.
-    # Aluno B implementará uma lógica baseada em crescimento exponencial.
-    # A dupla deve resolver o Merge Conflict na mantendo a segurança ética dos dados.
-    
-    focos_projetados = focos_atuais * taxa_reproducao
+def calcular_projecao_focos(focos_atuais, taxa_reproducao, periodos):
+    print("--- MONITORAMENTO DE ENDEMIAS UniFAP ---")
+
+    # Crescimento linear
+    focos_projetados = focos_atuais + (taxa_reproducao * periodos)
+
     return focos_projetados
 
+
 if __name__ == "__main__":
-    print(f"Total Projetado: {calcular_projecao_focos(10, 1.5)}")
+    print(f"Total Projetado: {calcular_projecao_focos(10, 1.5, 4)}")
